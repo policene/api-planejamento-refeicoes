a52308c803da19ec30f69bbff2e7aea243ecbcd7
Giovanni Policene <giovannicpoli@gmail.com>
2025-04-25 15:02:00 -0300

    feat: RedisConfig

---
+++ src/main/java/com/positivo/api_planejamento_refeicao/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.positivo.api_planejamento_refeicao.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+
+import java.time.Duration;
+
+import static org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer;
+
+
+@EnableCaching
+@Configuration
+public class RedisConfig {
+
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues()
+                .serializeValuesWith(fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+}
