bf3bee251a793512f593d40302ee8e5b0e27583b
Giovanni Policene <giovannicpoli@gmail.com>
2025-04-25 14:55:19 -0300

    initial commit

---
+++ src/main/java/com/positivo/api_planejamento_refeicao/ApiPlanejamentoRefeicaoApplication.java
@@ -0,0 +1,13 @@
+package com.positivo.api_planejamento_refeicao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPlanejamentoRefeicaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPlanejamentoRefeicaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/positivo/api_planejamento_refeicao/ApiPlanejamentoRefeicaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.positivo.api_planejamento_refeicao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPlanejamentoRefeicaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
